2ad6c2415f6d25388b7db217a16c1f75ca0a91e8
Incodame <tanguy.ronan@gmail.com>
2020-05-28 23:15:33 +0200

    Test doc_t

--- paragraph.sh
+++ paragraph.sh
@@ -1,7 +1,12 @@
 echo "Welcome to Paragraph. "
 echo ""
+echo "Start a working session with:"
 echo "?- consult('/opt/paragraph/prolog/paragraph_conf.pl')."
 echo ""
 echo "?- consult('/opt/paragraph/prolog/paragraph.pl')."
 echo ""
+echo "Load and execute a test suite such as doc_t with:"
+echo "?- consult('/opt/paragraph/t/doc_t.pl')."
+echo ""
+echo "?- run_tests."
 docker run -it -v $(pwd):/opt/paragraph incodame/swipl_images:om_search
