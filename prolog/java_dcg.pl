096e5aec0ccd5df409fec152fb627d190fc78f7d
Incodame <tanguy.ronan@gmail.com>
2024-12-29 18:31:43 +0100

    visibility modifier in java_dcg

--- prolog/java_dcg.pl
+++ prolog/java_dcg.pl
@@ -1,8 +1,9 @@
 :- module(java_dcg, [java_class_decl//4, java_class_file_content//1]).
 :- use_module(library(dcg/basics)).
 
+
 java_class_decl(class(C,As), Fields, Constructors, Methods) -->
-    java_annotations(As), "public class ", string_without(" ", C), { writeln(C) }, 
+    java_annotations(As), java_visibility_modifier(_), "class ", string_without(" ", C), { writeln(C) }, 
     " {", blanks,
     java_field_decls(Fields), blanks,
     java_constructor_decls(Constructors), blanks,
@@ -14,14 +15,19 @@ java_field_decls([Field|Fields]) -->
     java_field_decl(Field), blanks,
     java_field_decls(Fields).
 
-java_field_decl(field(F,As)) -->  blanks, java_annotations(As), "private ", string(F), ";", blanks, { writeln(F) }.
+java_field_decl(field(F,As)) -->  java_annotations(As), java_visibility_modifier(_), string_without(";", F), ";", blanks, { writeln(F) }.
+
+java_visibility_modifier(Modifier) --> ( "public", { ModifierStr = "public"} 
+                                       | "private", { ModifierStr = "private"} 
+                                       | "protected", { ModifierStr = "protected"}
+                                       ), blanks, { string_codes(ModifierStr, Modifier) }.
 
 java_annotations([]) --> [].
 java_annotations([Annotation|Annotations]) -->
     java_annotation(Annotation), blanks,
     java_annotations(Annotations).
 
-java_annotation(A) --> "@", string(A), blanks.
+java_annotation(A) --> "@", string_without(" ", A), blanks.
 
 java_constructor_decls([]) --> [].
 java_constructor_decls([Constructor|Constructors]) -->
@@ -33,13 +39,13 @@ java_method_decls([Method|Methods]) -->
     java_method_decl(Method), blanks,
     java_method_decls(Methods).
 
-java_constructor_decl(C) --> blanks, "public ", string(C), "(", java_parameter_list(_), ")", blanks, "{", blanks, "}", blanks.
+java_constructor_decl(C) --> blanks, java_visibility_modifier(_), string(C), "(", java_parameter_list(_), ")", blanks, "{", blanks, "}", blanks.
 
 java_parameter_list([]) --> [].
 java_parameter_list([P|Ps]) --> java_parameter(P), ",", java_parameter_list(Ps).
 java_parameter(P) --> blanks, "final ", string(P), blanks.
 
-java_method_decl(M) --> blanks, "public ", string(M), "(", java_parameter_list(_), ")", blanks, "{", blanks, "}", blanks.
+java_method_decl(M) --> blanks, java_visibility_modifier(_), string(M), "(", java_parameter_list(_), ")", blanks, "{", blanks, "}", blanks.
 
 java_class_file_content(Class) -->
     java_package_decl(_), blanks,
@@ -59,7 +65,7 @@ java_import_statement(Import) --> "import", blanks, string(Import), ";", blanks.
 test1(Class) :-
     Data = "public class MyClass { private String name; }",
     string_codes(Data, Codes),
-    phrase(java_class_decl(class(Class, []), [ field('name', []) ], _, _), Codes).
+    phrase(java_class_decl(class(Class, []), [ field('String name', []) ], _, _), Codes).
 
 test2(Class) :-
     phrase_from_file(java_class_file_content(class(Class, _)), '/opt/paragraph/ParagraphUI/src/main/java/org/incodame/paragraph/sample/webapp/demo/Parameter.java').
