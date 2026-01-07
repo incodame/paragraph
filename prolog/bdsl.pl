38139c28b302444bc1ef1f7e2c69bdf74c0c172d
Incodame <tanguy.ronan@gmail.com>
2026-01-07 23:29:07 +0100

    Changed operator . to :>

--- prolog/bdsl.pl
+++ prolog/bdsl.pl
@@ -2,34 +2,34 @@
 %% these facts are generated from the pavements definitions but can also be written by hand in the paragraph REPL
 %%   and from the application deployment context specified in paramv
 %% their structure is of the form:  
-%%   container_term . p(path_list)                   for specifying where to locate a container in the application structure
+%%   container_term :> p(path_list)                   for specifying where to locate a container in the application structure
 %%   container_term -+ param_term(param_props_list)  for specifying parameters associated to a container
 
-:- op(500, xfy, '.').
+:- op(500, xfy, ':>').
 :- op(400, xfy, '-+').
-:- discontiguous '.'/1.
+:- discontiguous ':>'/1.
 :- discontiguous '-+'/1.
 :- discontiguous p/1.
 :- discontiguous d/2.
 :- discontiguous z/2.
 
-f('pom.xml') . p([ c, dev, git, app ]).
+f('pom.xml') :> p([ c, dev, git, app ]).
 d('src', [ '-- main',
            '---- java',
            '-- test',
            '---- java'
-         ]) . p([ c, dev, git, app ]).
+         ]) :> p([ c, dev, git, app ]).
 
 z('app.jar', [ f('pom.xml'),
                d('-- com/example', 
                   [ 'a.class',
                     'b.class'
                   ])
-             ]) . p([ 'webapps', 'WEB-INF', 'lib' ]).
+             ]) :> p([ 'webapps', 'WEB-INF', 'lib' ]).
 
-z('lib.jar', [ f('pom.xml') | _]) . p([ 'webapps', 'WEB-INF', 'lib' ]).
+z('lib.jar', [ f('pom.xml') | _]) :> p([ 'webapps', 'WEB-INF', 'lib' ]).
 
-p([ webapps | _]) . p([ opt, incodam, tomcat ]).
+p([ webapps | _]) :> p([ opt, incodam, tomcat ]).
 
 f('pom.xml') -+ s([name=parent_pom, loc=xpath('//project/parent'), doc="pom.xml parent"]).
 
